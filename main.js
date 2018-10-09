0ba9c9d0464b6667bc0240ccf13ca5299911f46b
Naman Thakur <namanthakur22@gmail.com>
2018-10-09 23:43:24 +0530

    Updated checkbox event function

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 function ref(){
   var textcontent = document.getElementById('editor').value;
+  if (document.getElementById('reem').style.display ==="none"){
   document.getElementById('live-viewer').srcdoc = textcontent;
-  document.getElementById("hide").style.display="none";
-}
-  
-function cbChanged(checkboxElem) {
-  if (checkboxElem.checked) {
-       document.getElementById('ree').disabled = true;
-  } else {
-      document.getElementById('ree').disabled = false;
   }
 }
+function refr(){
+  var textcontent = document.getElementById('editor').value;
+  document.getElementById('live-viewer').srcdoc = textcontent;
+}
+  
+function cbChanged(){
+    if (document.getElementById('reem').style.display === "none") {
+      document.getElementById("reem").style.display = "block";
+    }
+    else{
+      document.getElementById("reem").style.display = "none";
+    }
+  }
