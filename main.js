da1095009d8702ca368ba46b24920b9a3af44ce5
Sanskar Verma <sanskarv3@gmail.com>
2018-10-09 15:28:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 function ref(){
   var textcontent = document.getElementById('editor').value;
   document.getElementById('live-viewer').srcdoc = textcontent;
+  document.getElementById("hide").style.display="none";
+}
+  
+function cbChanged(checkboxElem) {
+  if (checkboxElem.checked) {
+       document.getElementById('ree').disabled = true;
+  } else {
+      document.getElementById('ree').disabled = false;
+  }
 }
