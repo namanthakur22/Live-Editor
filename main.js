614b0a25dfdd81d951faded73535ccdc24cc790e
Naman Thakur <namanthakur22@gmail.com>
2018-10-10 23:05:14 +0530

    Updated Functions related to night mode

--- main.js
+++ main.js
@@ -17,3 +17,21 @@ function cbChanged(){
       document.getElementById("reem").style.display = "none";
     }
   }
+function back(){
+  if (document.getElementById('colour').style.backgroundColor === "black"){
+    document.getElementById('colour').style.backgroundColor = "lightgrey";
+    document.getElementById('editor').style.backgroundColor = "white";
+    document.getElementById('editor').style.color = "black";
+    document.getElementById('myform').style.color = "black";
+    document.getElementById('live-viewer').style.color = "black";
+    document.getElementById('live-viewer').style.backgroundColor = "white";
+  }
+  else{
+    document.getElementById('colour').style.backgroundColor = "black";
+    document.getElementById('editor').style.backgroundColor = "dimgrey";
+    document.getElementById('editor').style.color = "white";
+    document.getElementById('myform').style.color = "white";
+    document.getElementById('live-viewer').style.color = "white";
+    document.getElementById('live-viewer').style.backgroundColor = "dimgrey";
+  }
+}
